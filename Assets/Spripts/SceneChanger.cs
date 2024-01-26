95bd810c2618760f1ddfe03be442d6fafeca81db
Csizmadia Márk <matemarcalek@gmail.com>
2024-01-26 10:14:56 +0100

    Revert "Revert "Menu+Transition""
    This reverts commit 05d0b1c435cd6401475dd9bb186f1f1d246def80.

---
+++ Assets/Spripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Animator transition;
+
+    
+    public void MoveToScene(string sceneName)
+    {
+        StartCoroutine(LoadLevel(sceneName).GetEnumerator());
+    }
+
+    IEnumerable LoadLevel(string sceneName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(2.0f);
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
